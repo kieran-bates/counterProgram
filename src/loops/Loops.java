722efdd75618f959b18caf97cbb52136bd8f6d9d
Kieran Bates <>
2019-10-09 13:14:19 -0400

    Third commit of counterProgram - comments and header added

--- src/loops/Loops.java
+++ src/loops/Loops.java
@@ -1,15 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Kieran Bates
+ * October 9, 2019
+ * This program counts from a number to a number by a number based on user input
  */
 
 package loops;
 
-/**
- *
- * @author kibat6204
- */
 import java.util.Scanner;
 
 public class Loops {
@@ -19,15 +15,16 @@ public class Loops {
      */
     public static void main(String[] args) {
         
-        int countFrom;
+        int countFrom; //declare variables
         int countUntil;
         int countBy;
         int programSelection;
         boolean programStop = false;
         Scanner input = new Scanner(System.in);
-    while(programStop == false)
+        
+    while(programStop == false) //loop until programStop is true
     {
-        System.out.println("COUNTER PROGRAM");
+        System.out.println("COUNTER PROGRAM"); //Display user options
         System.out.println("\nPlease enter your choice:");
         System.out.println("1 ... Count from 0 to 10 by 1");
         System.out.println("2 ... Count from 100 to 0 by 10");
@@ -35,22 +32,22 @@ public class Loops {
         System.out.println("4 ... Count from 6000 to 1000 by 1000");
         System.out.println("5 ... Terminate Program");
         System.out.print("\nEnter choice here --> ");
-        programSelection = input.nextInt();
+        programSelection = input.nextInt(); //wait for user input
         
-        if(programSelection == 1)
+        if(programSelection == 1) //if user selects 1
         {
-            countFrom = 0;
-            countUntil = 10;
-            countBy = 1;
+            countFrom = 0; //declare start number
+            countUntil = 10; //declare stop number
+            countBy = 1; //declare count interval
             
-            for(int i = countFrom; i<=countUntil; i=i+countBy)
+            for(int i = countFrom; i<=countUntil; i=i+countBy) //loop until stop number has been reached
             {
-                System.out.println(i);
+                System.out.println(i); //print output
             }
-            System.out.println("***COUNT COMPLETE!***");
+            System.out.println("***COUNT COMPLETE!***"); //inform user count is complete
         }
         
-        if(programSelection == 2)
+        if(programSelection == 2) //if user selects 2 follow the same process
         {
             countFrom = 100;
             countUntil = 0;
@@ -63,7 +60,7 @@ public class Loops {
             System.out.println("***COUNT COMPLETE!***");
         }
         
-        if(programSelection == 3)
+        if(programSelection == 3) //if user selects 3 follow the same process
         {
             countFrom = 50;
             countUntil = 500;
@@ -76,7 +73,7 @@ public class Loops {
             System.out.println("***COUNT COMPLETE!***");
         }
         
-        if(programSelection == 4)
+        if(programSelection == 4) //if user selects 4 follow the same process
         {
             countFrom = 6000;
             countUntil = 1000;
@@ -88,9 +85,9 @@ public class Loops {
             }
             System.out.println("***COUNT COMPLETE!***");
         }
-        if(programSelection == 5)
+        if(programSelection == 5) //if user selects 5
         {
-            programStop = true;
+            programStop = true; //set programStop to true - program terminates
         }
     }
     } 
